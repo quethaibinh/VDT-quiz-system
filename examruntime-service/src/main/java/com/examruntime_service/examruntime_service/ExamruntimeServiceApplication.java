d2d4a990507b01296af0f6cc8312e47295c9ed5f
quethaibinh <vuducthanh03072004@gmail.com>
2026-06-10 15:28:21 +0700

    add exam-service

---
+++ examruntime-service/src/main/java/com/examruntime_service/examruntime_service/ExamruntimeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.examruntime_service.examruntime_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamruntimeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamruntimeServiceApplication.class, args);
+	}
+
+}
---
+++ examruntime-service/src/test/java/com/examruntime_service/examruntime_service/ExamruntimeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examruntime_service.examruntime_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamruntimeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
