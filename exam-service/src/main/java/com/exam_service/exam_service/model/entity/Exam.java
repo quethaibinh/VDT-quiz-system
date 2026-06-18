3569844e683048902fd5835feb01636b067bc45b
quethaibinh <vuducthanh03072004@gmail.com>
2026-06-18 16:09:07 +0700

    add gitignore => frontend

--- exam-service/src/main/java/com/exam_service/exam_service/model/entity/Exam.java
+++ exam-service/src/main/java/com/exam_service/exam_service/model/entity/Exam.java
@@ -6,13 +6,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
 @Entity
 @Table(name = "exam")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Exam {
+public class Exam extends BaseEntity{
 
-
+    private String code;
+    private String title;
+    private String description;
+    private UUID subjectId;
+    private String subjectNameSnapshot;
+    private UUID createdByTeacherId;
+    private 
 
 }
